5f04c7ced5a31236f35d25bac5a145db4141cef5
maschwalb <maschwalb@gmail.com>
2025-04-21 18:39:31 -0300

    wrong way to use zustand and query

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,19 +1,29 @@
 import {Text, View} from "react-native";
 import {useQuery} from "@tanstack/react-query";
 import {getUsers} from "./api/user";
+import {useUserStore} from "./store/userStore";
+import {useEffect} from "react";
 
 const Entry = () => {
+
+    const { users, setUsers } = useUserStore();
 
     const { data } = useQuery({
         queryKey: ['users'],
         queryFn: () => getUsers(),
     });
 
-        return (data?.map((user) => (
-            <Text key={user.id}>
-                {`${user.id}: ${user.name}`}
-            </Text>
-        )))
+    useEffect(() => {
+        if (data) {
+            setUsers(data);
+        }
+    }, [data]);
+
+    return (users.map((user) => (
+        <Text key={user.id}>
+            {`${user.id}: ${user.name}`}
+        </Text>
+    )))
 }
 
 export default Entry;
